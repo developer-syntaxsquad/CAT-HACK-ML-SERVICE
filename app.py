5260f0f32af56324bf988aa2c5b056c3d482f40f
chiranjeevi <developer.chiranjeevim@gmail.com>
2025-07-18 10:09:22 +0530

    status: Final Commit

--- app.py
+++ app.py
@@ -5,24 +5,33 @@ from model import train_model, predict_average_time
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app)
+
 
 @app.route("/", methods=['GET'])
 def home():
     return jsonify({"message": "hello from ml server"}), 200
 
-@app.route("/predict", methods=['POST'])
+@app.route("/predict/", methods=['POST', 'OPTIONS'])
 def predict():
-    if(request.is_json):
+    if request.method == "OPTIONS":
+        response = jsonify({'message': 'CORS preflight success'})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
+        response.headers.add("Access-Control-Allow-Methods", "POST,OPTIONS")
+        return response, 200
+
+    if request.is_json:
         data = request.get_json()
         response = predict_average_time(data["machine_id"], data["operator_id"])
-        return jsonify({"time" : response})
+        return jsonify({"time": response})
     else:
-        return jsonify({"message" : "Please Pass JSON Data"})
+        return jsonify({"message": "Please Pass JSON Data"}), 400
 
 
 
 if __name__ == "__main__":
     #driver code
-    app.run(host="0.0.0.0", port="8080", debug=True)
     train_model()
+    app.run(host="0.0.0.0", port=8080, debug=True)
+    
